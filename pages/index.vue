8beea60b56397e8fb99a803e88b1c18412707b6d
Maria Gabriela Guarecuco <mgabriela.guarecuco@gmail.com>
2025-03-29 16:37:57 -0400

    feat: vertically center the content in the viewport height in the index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,56 @@
 <template>
-  <div>
-    <main class="mx-auto max-w-screen-xl px-4 sm:px-8 pt-32">
-      <div class="grid md:grid-cols-2 gap-14 items-center">
-        <div class="flex flex-col items-center md:items-start relative">
-          <ul v-if="$device.isDesktop" class="ouroboros absolute">
-            <li v-for="n in 48" :key="n"></li>
-          </ul>
-          <h1
-            class="mb-8 text-center md:text-left text-3xl sm:text-5xl font-bold text-neutral-800 dark:text-neutral-100 z-50 font-display"
+  <main
+    class="mx-auto max-w-screen-xl px-4 sm:px-8 pt-20 md:min-h-[calc(100vh-4rem)] md:flex md:items-center md:-mt-20"
+  >
+    <div class="grid md:grid-cols-2 gap-14 items-center">
+      <div class="flex flex-col items-center md:items-start relative">
+        <ul v-if="$device.isDesktop" class="ouroboros absolute">
+          <li v-for="n in 48" :key="n"></li>
+        </ul>
+        <h1
+          class="mb-8 text-center md:text-left text-3xl sm:text-5xl font-bold text-neutral-800 dark:text-neutral-100 z-50 font-display"
+        >
+          Build recursive logic puzzles in seconds
+        </h1>
+        <p
+          class="mb-8 text-center md:text-left text-black dark:text-white z-50"
+        >
+          Create, customize, and solve unique self-referential quizzes. Our
+          builder generates guaranteed-solvable quizzes for both human enjoyment
+          and AI evaluation.
+        </p>
+        <div class="flex gap-4 z-50">
+          <NuxtLink
+            to="/create"
+            aria-label="Start generating"
+            class="bg-green-500 hover:bg-green-600 text-black rounded-md font-medium px-4 py-2"
           >
-            Build recursive logic puzzles in seconds
-          </h1>
-          <p
-            class="mb-8 text-center md:text-left text-black dark:text-white z-50"
+            Start Creating
+          </NuxtLink>
+          <NuxtLink
+            to="/about"
+            aria-label="Learn more"
+            class="bg-neutral-100 dark:bg-neutral-900 hover:bg-neutral-200 dark:hover:bg-neutral-800 dark:text-neutral-200 border-2 border-neutral-700 dark:border-neutral-400 rounded-md font-medium px-4 py-2"
           >
-            Create, customize, and solve unique self-referential quizzes. Our
-            builder generates guaranteed-solvable quizzes for both human
-            enjoyment and AI evaluation.
-          </p>
-          <div class="flex gap-4 z-50">
-            <NuxtLink
-              to="/create"
-              aria-label="Start generating"
-              class="bg-green-500 hover:bg-green-600 text-black rounded-md font-medium px-4 py-2"
-            >
-              Start Creating
-            </NuxtLink>
-            <NuxtLink
-              to="/about"
-              aria-label="Learn more"
-              class="bg-neutral-100 dark:bg-neutral-900 hover:bg-neutral-200 dark:hover:bg-neutral-800 dark:text-neutral-200 border-2 border-neutral-700 dark:border-neutral-400 rounded-md font-medium px-4 py-2"
-            >
-              Learn more
-            </NuxtLink>
-          </div>
-        </div>
-
-        <div class="shadow-md shadow-neutral-400/70 dark:shadow-neutral-950/80">
-          <NuxtImg
-            src="/images/self-referential-quiz-builder-dark.png"
-            alt="Quiz builder interface"
-            class="w-full h-auto hidden dark:block"
-          />
-          <NuxtImg
-            src="/images/self-referential-quiz-builder-light.png"
-            alt="Quiz builder interface"
-            class="w-full h-auto dark:hidden"
-          />
+            Learn more
+          </NuxtLink>
         </div>
       </div>
-    </main>
-  </div>
+
+      <div class="shadow-md shadow-neutral-400/70 dark:shadow-neutral-950/80">
+        <NuxtImg
+          src="/images/self-referential-quiz-builder-dark.png"
+          alt="Quiz builder interface"
+          class="w-full h-auto hidden dark:block"
+        />
+        <NuxtImg
+          src="/images/self-referential-quiz-builder-light.png"
+          alt="Quiz builder interface"
+          class="w-full h-auto dark:hidden"
+        />
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
@@ -72,7 +72,7 @@ ul.ouroboros {
   height: $dim;
   width: $dim;
   position: absolute;
-  top: 40%;
+  top: 60%;
   left: 50%;
   transform: translate(-50%, -50%);
   z-index: 0;
